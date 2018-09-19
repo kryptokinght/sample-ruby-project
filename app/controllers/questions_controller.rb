8975767422c17d8b5b8e59f5c0320c3b6e666bb5
kryptokinght <minanshu.1998@gmail.com>
2018-09-19 18:38:48 +0530

    saving questions to databse

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,12 @@
 class QuestionsController < ApplicationController
-    def index
+    def create
+        Question.create(question_params)
+        redirect_to root_path
     end
 
-	def about
-	end
+    private
+
+    def question_params
+        params.require(:question).permit(:email, :body)
+    end
 end
